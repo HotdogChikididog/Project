42533e22c7cea773ca98f0dac42f8ad5b4bfc873
william <williamcorotan@gmail.com>
2024-01-14 22:51:10 +0800

    added get_metadata function for admin

--- Private_Dashboard/include/connection.php
+++ Private_Dashboard/include/connection.php
@@ -4,4 +4,22 @@ $conn = mysqli_connect("localhost:3306","root","","file_management");
 if(!$conn){
 	die("Connection error: " . mysqli_connect_error());	
 }
+
+if(!function_exists('get_metadata'))
+{
+	function get_metadata($slug)
+	{
+		$table = "metadata";
+		
+		$statement = $conn->prepare("SELECT * FROM $table WHERE slug = ?");
+		$statement->bind_param("s", $slug);
+		$statement->execute();
+		$result = $statement->get_result();
+		$data = $result->fetch_assoc();
+		$statement->close();
+		$conn->close();
+	
+		return empty($data) ? 0 : 1;
+	}
+}
 ?>
